512586aaf06349342dc548d90a783dd5307883d9
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-13 23:38:39 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    session[:username] = params[:username]
-    redirect_to '/'
+    if params[:username].nil? || params[:username].empty?
+      redirect_to '/new'
+    else
+      session[:username] = params[:username]
+      redirect_to '/'
+    end
   end
 
   def destroy
     session.delete :username
+    redirect_to '/new'
   end
 end
